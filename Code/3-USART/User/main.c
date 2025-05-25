7ba12579f0ca9092bd230546951990d771f7edeb
mika <mikasa2511592510@gmail.com>
2025-05-25 13:23:38 +0800

    串口作为中间件处理

--- Code/3-USART/User/main.c
+++ Code/3-USART/User/main.c
@@ -3,13 +3,12 @@
 #include "systick.h"
 #include <stdio.h>
 #include "main.h"
-#include "USART.h"
+#include "Usart0.h"
 
 int main(void)
 {
     systick_config();
-    usart_config(); //配置串口
-    uint16_t cnt = 0;
+
     while(1) {
         //printf("Hello World! %d\n", cnt++); //打印数据
         //delay_1ms(1000); //延时1秒
